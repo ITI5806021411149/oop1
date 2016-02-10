39615ea6e91fdccf888ba3125e1c1620dfef86ec
SodaBarBeer <SodaBarBeer@Beer>
2016-02-11 01:02:53 +0700

    seven dwarves

---
+++ seven_Dwarves/src/seven_Dwarves.java
@@ -0,0 +1,101 @@
+import java.util.Scanner;
+public class seven_Dwarves {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int human1,human2,human3,human4,human5,human6,human7,human8,human9;
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter Dwarf 1 : ");
+		human1 = scan.nextInt();
+		System.out.print("Enter Dwarf 2 : ");
+		human2 = scan.nextInt();
+		System.out.print("Enter Dwarf 3 : ");
+		human3 = scan.nextInt();
+		System.out.print("Enter Dwarf 4 : ");
+		human4 = scan.nextInt();
+		System.out.print("Enter Dwarf 5 : ");
+		human5 = scan.nextInt();
+		System.out.print("Enter Dwarf 6 : ");
+		human6 = scan.nextInt();
+		System.out.print("Enter Dwarf 7 : ");
+		human7 = scan.nextInt();
+		System.out.print("Enter Dwarf 8 : ");
+		human8 = scan.nextInt();
+		System.out.print("Enter Dwarf 9 : ");
+		human9 = scan.nextInt();
+		System.out.println();
+		if(human3+human4+human5+human6+human7+human8+human9==100)
+		System.out.print("Fake Dwarf is Dwarf1 & Dwarf2");
+		if(human2+human4+human5+human6+human7+human8+human9==100)
+		System.out.print("Fake Dwarf is Dwarf1 & Dwarf3");
+		if(human3+human2+human5+human6+human7+human8+human9==100)
+		System.out.print("Fake Dwarf is Dwarf1 & Dwarf4");
+		if(human3+human4+human2+human6+human7+human8+human9==100)
+		System.out.print("Fake Dwarf is Dwarf1 & Dwarf5");
+		if(human3+human4+human5+human2+human7+human8+human9==100)
+		System.out.print("Fake Dwarf is Dwarf1 & Dwarf6");
+		if(human3+human4+human5+human6+human2+human8+human9==100)
+		System.out.print("Fake Dwarf is Dwarf1 & Dwarf7");
+		if(human3+human4+human5+human6+human7+human2+human9==100)
+		System.out.print("Fake Dwarf is Dwarf1 & Dwarf8");
+		if(human3+human4+human5+human6+human7+human8+human2==100)
+		System.out.print("Fake Dwarf is Dwarf1 & Dwarf9");
+		if(human1+human4+human5+human6+human7+human8+human9==100)
+		System.out.print("Fake Dwarf is Dwarf2 & Dwarf3");
+		if(human3+human1+human5+human6+human7+human8+human9==100)
+		System.out.print("Fake Dwarf is Dwarf2 & Dwarf4");
+		if(human3+human4+human1+human6+human7+human8+human9==100)
+		System.out.print("Fake Dwarf is Dwarf2 & Dwarf5");
+		if(human3+human4+human5+human1+human7+human8+human9==100)
+		System.out.print("Fake Dwarf is Dwarf2 & Dwarf6");
+		if(human3+human4+human5+human6+human1+human8+human9==100)
+		System.out.print("Fake Dwarf is Dwarf2 & Dwarf7");
+		if(human3+human4+human5+human6+human7+human1+human9==100)
+		System.out.print("Fake Dwarf is Dwarf2 & Dwarf8");
+		if(human3+human4+human5+human6+human7+human8+human1==100)
+		System.out.print("Fake Dwarf is Dwarf2 & Dwarf9");
+		if(human1+human2+human5+human6+human7+human8+human9==100)
+		System.out.print("Fake Dwarf is Dwarf3 & Dwarf4");
+		if(human1+human4+human2+human6+human7+human8+human9==100)
+		System.out.print("Fake Dwarf is Dwarf3 & Dwarf5");
+		if(human1+human4+human5+human2+human7+human8+human9==100)
+		System.out.print("Fake Dwarf is Dwarf3 & Dwarf6");
+		if(human1+human4+human5+human6+human2+human8+human9==100)
+		System.out.print("Fake Dwarf is Dwarf3 & Dwarf7");
+		if(human1+human4+human5+human6+human7+human2+human9==100)
+		System.out.print("Fake Dwarf is Dwarf3 & Dwarf8");
+		if(human1+human4+human5+human6+human7+human8+human2==100)
+		System.out.print("Fake Dwarf is Dwarf3 & Dwarf9");
+		if(human3+human1+human2+human6+human7+human8+human9==100)
+		System.out.print("Fake Dwarf is Dwarf4 & Dwarf5");
+		if(human3+human1+human5+human2+human7+human8+human9==100)
+		System.out.print("Fake Dwarf is Dwarf4 & Dwarf6");
+		if(human3+human1+human5+human6+human2+human8+human9==100)
+		System.out.print("Fake Dwarf is Dwarf4 & Dwarf7");
+		if(human3+human1+human5+human6+human7+human2+human9==100)
+		System.out.print("Fake Dwarf is Dwarf4 & Dwarf8");
+		if(human3+human1+human5+human6+human7+human8+human2==100)
+		System.out.print("Fake Dwarf is Dwarf4 & Dwarf9");
+		if(human3+human4+human1+human2+human7+human8+human9==100)
+		System.out.print("Fake Dwarf is Dwarf5 & Dwarf6");
+		if(human3+human4+human1+human6+human2+human8+human9==100)
+		System.out.print("Fake Dwarf is Dwarf5 & Dwarf7");
+		if(human3+human4+human1+human6+human7+human2+human9==100)
+		System.out.print("Fake Dwarf is Dwarf5 & Dwarf8");
+		if(human3+human4+human1+human6+human7+human8+human2==100)
+		System.out.print("Fake Dwarf is Dwarf5 & Dwarf9");
+		if(human3+human4+human5+human1+human2+human8+human9==100)
+		System.out.print("Fake Dwarf is Dwarf6 & Dwarf7");
+		if(human3+human4+human5+human1+human7+human2+human9==100)
+		System.out.print("Fake Dwarf is Dwarf6 & Dwarf8");
+		if(human3+human4+human5+human1+human7+human8+human2==100)
+		System.out.print("Fake Dwarf is Dwarf6 & Dwarf9");
+		if(human3+human4+human5+human6+human1+human2+human9==100)
+		System.out.print("Fake Dwarf is Dwarf7 & Dwarf8");
+		if(human3+human4+human5+human6+human1+human8+human2==100)
+		System.out.print("Fake Dwarf is Dwarf7 & Dwarf9");
+		if(human3+human4+human5+human6+human7+human1+human2==100)
+		System.out.print("Fake Dwarf is Dwarf8 & Dwarf9");
+	}
+
+}
